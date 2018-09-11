72f45014aff1010312a184d781be91a2dd162782
karanwadhwa <wadhwa.karan96@gmail.com>
2018-09-11 19:46:37 +0530

    basic express server

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const app = express();
+const port = 5000;
+
+app.get("/", (req, res) => res.send(`Server running on port ${port}`));
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
