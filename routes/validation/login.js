c7f8369737d83981a85db9b4843d0ce452a34ab9
karanwadhwa <wadhwa.karan96@gmail.com>
2018-09-17 23:30:21 +0530

    tweaked login validation

--- routes/validation/login.js
+++ routes/validation/login.js
@@ -1,6 +1,8 @@
 const validator = require("validator");
 const isEmpty = require("./isEmpty");
 
+const { EMAIL_DOMAIN, REG_NO_LOWER_LIMIT, REG_NO_UPPER_LIMIT } = process.env;
+
 module.exports = validateLoginInput = data => {
   let errors = {};
 
@@ -8,8 +10,18 @@ module.exports = validateLoginInput = data => {
   data.password = !isEmpty(data.password) ? data.password : "";
 
   // Check username field
-  if (validator.isEmpty(data.username)) {
+  if (
+    !validator.isInt(data.username, {
+      min: REG_NO_LOWER_LIMIT,
+      max: REG_NO_UPPER_LIMIT
+    }) &&
+    !validator.isEmail(data.username) &&
+    !data.username.toLowerCase().includes(EMAIL_DOMAIN)
+  ) {
     errors.username = "Enter your email or registration number as username";
+  }
+  if (validator.isEmpty(data.username)) {
+    errors.username = "Username is required";
   }
 
   // Check password field
