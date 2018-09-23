f885b3709f583697b521109b10857186713ffb5d
karanwadhwa <wadhwa.karan96@gmail.com>
2018-09-23 19:43:38 +0530

    StudentProfile model

---
+++ Models/StudentProfile.js
@@ -0,0 +1,131 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const StudentProfileSchema = new Schema({
+  // _id key from users collection
+  userKey: {
+    type: Schema.Types.ObjectId,
+    ref: "users",
+    required: true,
+    unique: true
+  },
+
+  // userID - Registration No / StaffID
+  userID: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  smartCardID: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  // no idea why this is here
+  // keeping it around anyway
+  userType: {
+    type: String,
+    //required: true,
+    default: "Student"
+  },
+
+  // mod access
+  moderator: {
+    type: Boolean,
+    default: false
+  },
+
+  bio: String,
+
+  phone: Number,
+
+  badges: [String],
+
+  // mentor details
+  mentor: {
+    name: {
+      type: String,
+      required: true
+    },
+    userID: {
+      type: String,
+      required: true
+    }
+  },
+
+  courseDetails: {
+    department: {
+      type: String,
+      required: true
+    },
+    year: {
+      type: String,
+      required: true
+    },
+    class: {
+      type: String,
+      required: true
+    },
+    rollNo: {
+      type: Number,
+      required: true
+    },
+    batch: {
+      type: String,
+      required: true
+    },
+    elective: {
+      type: String,
+      default: null
+    },
+    cr: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  subscriptions: {
+    type: Array,
+    required: true,
+    default: ["public"]
+  },
+
+  studentCouncils: {
+    IEEE: {
+      type: String,
+      default: null
+    },
+    CSI: {
+      type: String,
+      default: null
+    },
+    IETE: {
+      type: String,
+      default: null
+    },
+    ACM: {
+      type: String,
+      default: null
+    },
+    ISTE: {
+      type: String,
+      default: null
+    },
+    ECELL: {
+      type: String,
+      default: null
+    }
+  }
+
+  /* blocked: {
+    type: Boolean,
+    default: false
+  } */
+});
+
+module.exports = StudentProfile = mongoose.model(
+  "student-profiles",
+  StudentProfileSchema
+);
