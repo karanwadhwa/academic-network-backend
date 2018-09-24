a3289e3c2b3a5572b5631cc918b9c6542d4920f1
karanwadhwa <wadhwa.karan96@gmail.com>
2018-09-24 14:49:32 +0530

    GET api/profile route to fetch currently logged in users profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,9 @@
 const express = require("express");
+const passport = require("passport");
+
+// Import Models
+const User = require("../../models/User");
+const StudentProfile = require("../../models/StudentProfile");
 
 const router = express.Router();
 
@@ -8,5 +13,26 @@ const router = express.Router();
 router.get("/test", (req, res) => {
   res.json({ msg: "/api/profile/test route" });
 });
+
+// @route   GET /api/profile
+// @desc    fetches currently logged in users profile
+// @access  Protected
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+
+    StudentProfile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = "Profile not found.";
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(400).json(err));
+  }
+);
 
 module.exports = router;
