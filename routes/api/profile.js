8de1e2fdf09e725b8236ed62ecc1f9c1ae4784a7
karanwadhwa <wadhwa.karan96@gmail.com>
2018-12-17 00:12:40 +0530

    POST /api/profile/create/professor basic ProfessorProfile creation route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,7 @@ const passport = require("passport");
 // Import Models
 const User = require("../../models/User");
 const StudentProfile = require("../../models/StudentProfile");
+const ProfessorProfile = require("../../models/ProfessorProfile");
 
 const router = express.Router();
 
@@ -122,6 +123,58 @@ router.post(
         return res.status(400).json(errors);
       }
       new StudentProfile(profileFields)
+        .save()
+        .then(profile => res.status(201).json(profile))
+        .catch(err => res.status(400).json(err));
+    });
+  }
+);
+
+// @route   POST /api/profile/create/professor
+// @desc    creates professor profile
+// @access  Protected
+router.post(
+  "/create/professor",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    //check usertype
+    if (req.user.userType !== "professor") {
+      return res.status(404).json({
+        error:
+          "invalid user type. User needs to be a professor to access this route"
+      });
+    }
+
+    // This route only creates a basic professor profile
+    // Separate route for education field
+    // Separate route for mentees if any
+
+    const profileFields = {};
+    profileFields.subscriptions = ["public", "professors"];
+
+    // Set the userKey and userID from User model stored in req.user
+    profileFields.userKey = req.user.id;
+    profileFields.userID = req.user.userID;
+
+    // optional fields
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.phone) profileFields.phone = req.body.phone;
+
+    profileFields.department = req.body.department;
+    profileFields.designation = req.body.designation;
+
+    // push subscriptions
+    profileFields.subscriptions.push(req.body.department);
+
+    ProfessorProfile.findOne({ userKey: req.user.id }).then(profile => {
+      if (profile) {
+        const errors = {
+          existingProfile:
+            "profile for this user already exists, try update route"
+        };
+        return res.status(400).json(errors);
+      }
+      new ProfessorProfile(profileFields)
         .save()
         .then(profile => res.status(201).json(profile))
         .catch(err => res.status(400).json(err));
