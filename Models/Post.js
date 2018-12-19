3513960561db7cbf09df3844f06b7f8c496633f7
karanwadhwa <wadhwa.karan96@gmail.com>
2018-12-19 13:24:55 +0530

    added audience to Post model

--- Models/Post.js
+++ Models/Post.js
@@ -31,6 +31,13 @@ const PostSchema = new Schema({
     default: DEFAULT_AVATAR
   },
 
+  audience: [
+    {
+      type: String,
+      required: true
+    }
+  ],
+
   date: {
     type: Date,
     required: true,
