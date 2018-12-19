47111a9c85c5806feaae009613b4014be19efe0c
karanwadhwa <wadhwa.karan96@gmail.com>
2018-12-19 15:53:21 +0530

    POST /api/posts/ basic post creation route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const passport = require("passport");
+
+// Import Models
+const Post = require("../../models/Post");
 
 const router = express.Router();
 
@@ -8,5 +12,25 @@ const router = express.Router();
 router.get("/test", (req, res) => {
   res.json({ msg: "/api/posts/test route" });
 });
+
+// @route   POST /api/posts/test
+// @desc    create post
+// @access  Protected
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const newPost = new Post({
+      title: req.body.title,
+      body: req.body.body,
+      audience: req.body.audience,
+      author: `${req.user.fname} ${req.user.lname}`.trim(),
+      userKey: req.user.id,
+      avatar: req.user.avatar
+    });
+
+    newPost.save().then(post => res.status(201).json(post));
+  }
+);
 
 module.exports = router;
